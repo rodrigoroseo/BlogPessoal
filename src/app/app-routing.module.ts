cba1b0654b14732b036cab3a3289cb55c588cbcc
rodrigoroseo <rodrigo.roseo@gmail.com>
2021-09-27 10:21:11 -0300

    rotas

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,13 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { CadastrarComponent } from './cadastrar/cadastrar.component';
+import { EntrarComponent } from './entrar/entrar.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path:'entrar', component: EntrarComponent},
+  {path:'cadastrar', component: CadastrarComponent},
+  {path:'',redirectTo: 'entrar', pathMatch:'full'}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
