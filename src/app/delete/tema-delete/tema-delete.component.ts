f8739cbbce249fc6b8d8794368f56e9e3c25160b
rodrigoroseo <rodrigo.roseo@gmail.com>
2021-10-13 09:19:40 -0300

    Alertas reutilizaveis implementados

--- src/app/delete/tema-delete/tema-delete.component.ts
+++ src/app/delete/tema-delete/tema-delete.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute, Router } from '@angular/router';
 import { Tema } from 'src/app/model/Tema';
+import { AlertasService } from 'src/app/service/alertas.service';
 import { TemaService } from 'src/app/service/tema.service';
 import { environment } from 'src/environments/environment.prod';
 
@@ -17,12 +18,13 @@ export class TemaDeleteComponent implements OnInit {
   constructor(
     private temaService: TemaService,
     private router: Router,
-    private route: ActivatedRoute
+    private route: ActivatedRoute,
+    private alertas: AlertasService
   ) { }
 
   ngOnInit(){
     if(environment.token == ''){
-      alert('Sua seção expirou, faça o login novamente')
+      this.alertas.showAlertInfo('Sua seção expirou, faça o login novamente')
       this.router.navigate(['/entrar'])
     }
 
@@ -38,7 +40,7 @@ export class TemaDeleteComponent implements OnInit {
 
   apagar(){
     this.temaService.deleteTema(this.idTema).subscribe(() => {
-      alert('Tema apagado com sucesso')
+      this.alertas.showAlertSuccess('Tema apagado com sucesso')
       this.router.navigate(['/tema'])
     })
   }
--- src/app/edit/user-edit/user-edit.component.ts
+++ src/app/edit/user-edit/user-edit.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute, Router } from '@angular/router';
 import { User } from 'src/app/model/User';
+import { AlertasService } from 'src/app/service/alertas.service';
 import { AuthService } from 'src/app/service/auth.service';
 import { environment } from 'src/environments/environment.prod';
 
@@ -19,14 +20,15 @@ export class UserEditComponent implements OnInit {
   constructor(
     private authService: AuthService,
     private router: Router,
-    private route: ActivatedRoute
+    private route: ActivatedRoute,
+    private alertas: AlertasService
   ) { }
 
   ngOnInit(){
     window.scroll(0,0)
 
     if(environment.token == ''){
-      alert('Sua seção expirou, faça o login novamente')
+      this.alertas.showAlertInfo('Sua seção expirou, faça o login novamente')
       this.router.navigate(['/entrar'])
     }
 
@@ -46,12 +48,12 @@ export class UserEditComponent implements OnInit {
     this.user.tipo = this.tipoUsuario
 
     if(this.user.senha != this.confirmarSenha) {
-      alert('As senhas estão incorretas.')
+      this.alertas.showAlertDanger('As senhas estão incorretas.')
     } else {
       this.authService.atualizar(this.user).subscribe((resp: User) => {
         this.user = resp
         
-        alert('Usuário atualizado com sucesso, faça o login novamente')
+        this.alertas.showAlertSuccess('Usuário atualizado com sucesso, faça o login novamente')
 
         environment.id = 0
         environment.token = ''
